f6bd4e6757784bd2d04393a1cdfd7e897a327fba
cdj <728719296@qq.com>
2025-04-06 22:20:11 +0800

    初次提交

---
+++ src/main/java/com/cdj/Main.java
@@ -0,0 +1,12 @@
+package com.cdj;
+
+/**
+ * @author 陈登杰
+ * @create 2025-04-06 22:12
+ *///TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
